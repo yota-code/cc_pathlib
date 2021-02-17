2310979c9c9d2366ccd0b1b52b5b9074511c057b
yoochan <yota.news@gmail.com>
2021-02-17 15:42:10 +0100

    hardlink restored

--- package/cc_pathlib/path.py
+++ package/cc_pathlib/path.py
@@ -217,13 +217,12 @@ class Path(type(pathlib.Path())) :
 		else :
 			return data
 
-	# existe à partir de python 3.8
-	# def hardlink_at(self, link) :
-	# 	""" self is the file we link TO, link is the name FROM """
-	# 	if self.is_file() :
-	# 		os.link(self, link)
-	# 	else :
-	# 		raise ValueError("hardlink source must be a file")
+	def hardlink_to(self, target) :
+		""" self is the file we link FROM, link is the name TO """
+		if self.is_file() :
+			os.link(target, self)
+		else :
+			raise ValueError("hardlink source must be a file")
 
 	@property
 	def fname(self) :

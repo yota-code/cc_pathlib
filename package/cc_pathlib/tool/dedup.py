08863a01e07e5f1dd22cf75eb6bb169df9ac55be
yoochan <yota.news@gmail.com>
2021-11-23 17:09:35 +0100

    modification de dedup en cours pour qu'il soit plus propre

--- package/cc_pathlib/tool/dedup.py
+++ package/cc_pathlib/tool/dedup.py
@@ -2,7 +2,59 @@
 
 import collections
 
+class DedupDir() :
+	def __init__(self, base_dir) :
+		self.base_dir = base_dir
+
+	def build_name_map(self, path_set) :
+		name_map = collections.defaultdict(set)
+		for pth in path_set :
+			name_map[pth.name].add(pth)
+
+	def build_size_map(self, path_set) :
+		size_map = collections.defaultdict(set)
+		for pth in path_set :
+			size_map[pth.stat().st_size].add(pth)
+
+	def dedup_hash(self, path_set) :
+		hash_map = collections.defaultdict(set)
+		for pth in path_set :
+			hash_map[hash(pth.read_bytes())].add(pth)
+
+		for k in hash_map :
+			path_subset = hash_map[k]
+			if len(path_subset) :
+				self.dedup_file(path_subset)
+
+
+	def dedup_file(self, path_set) :
+		file_map = collections.defaultdict(set)
+		for pth in path_set :
+			file_map[pth.read_bytes()].add(pth)
+
+		for k in file_map :
+			path_subset = file_map[k]
+			if len(path_subset) :
+				self.fuse(path_subset)
+		
+	def fuse(self, path_set) :
+		# fuse is destructive, be careful to pass only strictly identical files
+		path_lst = sorted(path_set)
+		path_orig = path_lst.pop()
+		for pth in path_lst :
+			pth.unlink()
+			pth.hardlink_to(path_orig)
+			print(f"{pth} -> {pth_prev}")
+
+	def dedup(self, pattern='*') :
+		self.path_set = set()
+		for pth in self.base_dir.rglob(self.pattern) :
+			self.path_set.add(pth)
+
+
+
 def _dedup_size_pass(base_dir, pattern) :
+
 	size_map = collections.defaultdict(set)
 	for pth in base_dir.rglob(pattern) :
 		size_map[pth.stat().st_size].add(pth)
@@ -11,11 +63,9 @@ def _dedup_size_pass(base_dir, pattern) :
 
 def _dedup_checksum_pass(size_map) :
 	for k in reversed( sorted(size_map) ) :
-
 		pth_set = size_map[k]
 		if len(pth_set) <= 1 :
 			continue
-
 		checksum_map = collections.defaultdict(set)
 		for pth in pth_set :
 			checksum_map[ hash(pth.read_bytes()) ].add(pth)
@@ -55,7 +105,7 @@ def _dedup_inode_pass(content_map) :
 				inode_prev = inode_curr
 				pth_prev = pth
 
-def dedup(base_dir, pattern) :
+def dedup(base_dir, pattern='*.*') :
 	if base_dir.is_dir() :
 		_dedup_size_pass(base_dir, pattern)
 	else :

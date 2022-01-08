8f752192d99455185888e1fe4ec3ea8020daeff7
yoochan <yota.news@gmail.com>
2022-01-08 11:05:39 +0100

    added tests

---
+++ test/filter/cc_numpy/02/run.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import sys
+
+import numpy as np
+
+from cc_pathlib import Path
+
+u = np.mgrid[0:5,0:5]
+u = np.random.random((1440, 5)).astype(np.float32)
+
+pth = Path("test.npy.br")
+pth.save(u)
+
+v = pth.load()
+
+if (u == v).all() :
+	sys.exit(0)
+else :
+	print("ERROR")
+	sys.exit(1)

d4bf2462c9648833f4e031fbfdb2a27f059fd89b
yoochan <yota.news@gmail.com>
2023-04-22 11:16:16 +0200

    v0.0.17

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 setuptools.setup(
     name="cc-pathlib",
-    version="0.0.15",
+    version="0.0.17",
     author="Yoochan",
     author_email="yota.news@gmail.com",
     description="a supercharged implementation of pathlib.Path()",

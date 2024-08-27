14ef8598578ae4e8e835cc4d16ddb9eefb83fec9
yoochan <yota.news@gmail.com>
2024-08-27 16:12:00 +0200

    dedup rewrite

--- package/cc_pathlib/tool/dedup.py
+++ package/cc_pathlib/tool/dedup.py
@@ -4,6 +4,8 @@ from cc_pathlib import Path
 
 import collections
 
+import binascii
+
 """
 this code was not extensively tested, use with care
 """
@@ -23,46 +25,73 @@ class DedupDir() :
 			cache_pth = Path("/tmp/cc_pathlib") / (urllib.parse.quote(str(self.base_dir), safe='') + '.pickle')
 			print("cache_pth =", cache_pth)
 			if cache_pth.is_file() :
-				print("LoAD")
+				print("LOAD")
 				self.r_map = cache_pth.load()
 				return
 
 		self.r_map = dict()
-		self.r_map[None] = suffix_set
+		# self.r_map[None] = suffix_set
 
-		print("SCAN", suffix_set)
+		print("SCAN", suffix_set, end=" ")
 		for pth in self.base_dir.iter_recursive(* suffix_set) :
-			print(pth)
 			self.r_map[pth.relative_to(self.base_dir)] = pth.stat()
+		print(len(self.r_map))
 
 		if cache :
+			print("SAVE")
 			print(self.r_map)
 			cache_pth.save(self.r_map)
 
+	def hash(self, pth) :
+		return binascii.crc32((self.base_dir / pth).read_bytes())
+
 	def dedup_name(self, pth_set) :
+		print(f"= dedup_name(... {len(pth_set) if pth_set is not None else pth_set})")
+
+		if pth_set is None :
+			pth_set = set(self.r_map)
+
 		name_map = collections.defaultdict(set)
 		for pth in pth_set :
 			name_map[pth.name].add(pth)
 
 		for k in name_map :
 			path_subset = name_map[k]
-			if len(path_subset) :
+			if 1 < len(path_subset) :
 				self.dedup_size(path_subset)
 
-	def dedup_size(self, pth_set) :
+	def dedup_size(self, pth_set=None) :
+		print(f"=   dedup_size(... {len(pth_set) if pth_set is not None else pth_set})")
+		if pth_set is None :
+			pth_set = set(self.r_map)
 		size_map = collections.defaultdict(set)
 		for pth in pth_set :
-			size_map[pth.stat().st_size].add(pth)
+			size_map[self.r_map[pth].st_size].add(pth)
 
 		for k in size_map :
-			path_subset = size_map[k]
-			if len(path_subset) :
-				self.dedup_hash(path_subset)
+			size_set = size_map[k]
+			if 1 < len(size_set) :
+				self.dedup_inode(size_set)
+
+	def dedup_inode(self, pth_set) :
+		""" the input is a set of pth with the same size
+		we keep only one per inode
+		(because all files linked to the same inode are already hardlinked)
+		"""
+		print(f"=     dedup_inode(... {len(pth_set)})")
+		inode_map = collections.defaultdict(list)
+		for pth in pth_set :
+			inode_map[self.r_map[pth].st_ino].append(pth)
+
+		inode_set = set(inode_map[k][0] for k in inode_map)
+		if 1 < len(inode_set) :
+			self.dedup_hash(inode_set)
 
 	def dedup_hash(self, pth_set) :
+		print(f"=       dedup_hash(... {len(pth_set)})")
 		hash_map = collections.defaultdict(set)
 		for pth in pth_set :
-			hash_map[hash(pth.read_bytes())].add(pth)
+			hash_map[self.hash(pth)].add(pth)
 
 		for k in hash_map :
 			path_subset = hash_map[k]
@@ -70,30 +99,32 @@ class DedupDir() :
 				self.dedup_file(path_subset)
 
 	def dedup_file(self, pth_set) :
+		print(f"=         dedup_file(... {len(pth_set)})")
 		file_map = collections.defaultdict(set)
 		for pth in pth_set :
-			file_map[pth.read_bytes()].add(pth)
+			file_map[(self.base_dir / pth).read_bytes()].add(pth)
 
 		for k in file_map :
-			path_subset = file_map[k]
-			if len(path_subset) :
-				self.fuse(path_subset)
+			file_set = file_map[k]
+			if 1 < len(file_set) :
+				self.fuse(file_set)
 		
 	def fuse(self, pth_set) :
 		# fuse is destructive, be careful to pass to this stage only strictly identical files
 		pth_lst = sorted(pth_set)
 
 		pth_orig = pth_lst.pop()
-		inode_orig = pth_orig.stat().st_ino
+		inode_orig = self.r_map[pth_orig].st_ino
 
+		print(f"\t{pth_orig}")
 		for pth_test in pth_lst :
-			inode_test = pth_test.stat().st_ino
+			inode_test = self.r_map[pth_test].st_ino
 			if inode_test != inode_orig :
 				if self.dry_run :
 					pass
 					# pth_test.unlink()
 					# pth_test.hardlink_to(pth_orig)
-				print(f"{pth_test} -> {pth_orig}")
+				print(f"\t  <- {pth_test}")
 
 	def dedup(self) :
 		if self.only_same_name :
--- test/tool/dedup/generate_fake_files.py
+++ test/tool/dedup/generate_fake_files.py
@@ -13,11 +13,29 @@ tmp_dir.delete_content()
 
 letter = list('ABCDEHJKMPRSTXY')
 
+h_lst = list()
+
 for i in range(4096) :
 	random.shuffle(letter)
 	p = [letter[i] for i in range(0,random.randrange(2,4))]
-	p[-1] += '.txt'
-	q = letter[-1]
+	p[-1] += ''.join(letter[5:9]) + '.txt'
+	q = ''.join(letter[-random.randrange(1,3):])
+
+	if i % 8 == 0 :
+		h_lst.append(tmp_dir / Path(* p))
 
 	(tmp_dir / Path(* p)).make_parents()
 	(tmp_dir / Path(* p)).write_text(q)
+
+for h in h_lst :
+	random.shuffle(letter)
+	p = [letter[i] for i in range(0,random.randrange(2,4))]
+	p[-1] += ''.join(letter[5:9]) + '.txt'
+
+	if (tmp_dir / Path(* p)) == h :
+		continue
+
+	if (tmp_dir / Path(* p)).is_file() :
+		(tmp_dir / Path(* p)).unlink()
+	(tmp_dir / Path(* p)).make_parents()
+	(tmp_dir / Path(* p)).hardlink_to(h)

609739f23d37f265f2854a49713a20b8d9d03fee
yoochan <yota.news@gmail.com>
2024-09-03 15:16:52 +0200

    wip

--- upload_to_pypi.sh
+++ upload_to_pypi.sh
@@ -7,9 +7,9 @@ python3 setup.py sdist bdist_wheel
 
 if [[ $1 == '--release' ]]
 then
-	python3 -m twine upload dist/*
+	python3 -m twine upload --verbose --repository cc-pathlib_REL dist/*
 else
-	python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+	python3 -m twine upload --verbose --repository cc-pathlib_TST dist/*
 	if [[ $? == 0 ]]
 	then
 		echo "you can now upload officially with the flag --release"

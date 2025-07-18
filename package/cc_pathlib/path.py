1653ced5c17be0598fd6d5119897a6c4e2854c0e
clément chassagne <clement.c.chassagne@airbus.com>
2025-07-18 15:20:09 +0200

    mako

--- package/cc_pathlib/path.py
+++ package/cc_pathlib/path.py
@@ -399,8 +399,10 @@ class Path(type(pathlib.Path())) :
 			) :
 				return smap == omap
 
-	def mako(self, dst_dir=None, src_dir=None, param=None) :
+	def mako(self, dst_dir=None, src_dir=None, *, param=None) :
 		assert self.suffix == '.mako'
+
+		import mako.template
 
 		if dst_dir is None :
 			dst_dir = self.parent
@@ -408,18 +410,22 @@ class Path(type(pathlib.Path())) :
 		if src_dir is None :
 			src_dir = self.parent
 
-		if mako.is_file() :
+		if self.is_file() :
 			src_pth = self.relative_to(src_dir)
 			dst_pth = dst_dir / src_pth.with_suffix('')
 
 			mtf = mako.template.Template(filename=str(self))
 			dst_pth.make_parents()
+			print(f"{src_pth} -> {dst_pth}")
 			try :
 				dst_pth.write_text(mtf.render(** param))
 			except Exception as e :
+				import traceback
 				dst_pth.write_text(f'Failed rendering ! {e}\n------\n{traceback.format_exc()}')
 
-		elif mako.is_dir() :
+			return dst_pth
+
+		elif self.is_dir() :
 			raise NotImplementedError
 
 		else :
